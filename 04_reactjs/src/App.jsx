b9d6bdae1696a5ad728be17a83431fb567758192
sanket4real <desaisanket68@gmail.com>
2026-01-29 08:37:43 +0530

    updated react component

--- 04_reactjs/src/App.jsx
+++ 04_reactjs/src/App.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import Car from "./components/car";
 
 const App = () => {
-	return <div>App</div>;
+	return (
+		<div>
+			<h1>Welcome To The Car Store</h1>
+
+			<ul>
+				<Car />
+				<Car />
+				<Car />
+			</ul>
+		</div>
+	);
 };
 
 export default App;
